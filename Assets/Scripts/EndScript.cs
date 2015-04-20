e2ac2896f775049b089c0cfff03a2d7d2dcb5085
TheAsuro <theasuro@gmail.com>
2015-04-20 18:17:56 +0200

    End

---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScript : MonoBehaviour
+{
+    public float displayDuration;
+
+    float endTime;
+
+    void Awake()
+    {
+        endTime = Time.time + displayDuration;
+    }
+
+    void Update()
+    {
+        if (Time.time > endTime)
+        {
+            Application.LoadLevel(0);
+        }
+    }
+}
